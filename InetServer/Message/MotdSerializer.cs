510f3cb6f6f8592dfaf76344faab5953d7c2a9db
Anders Eriksson <aeri3@kth.se>
2016-06-03 22:44:27 +0200

    Updated message of the day functionality.

--- InetServer/Message/MotdSerializer.cs
+++ InetServer/Message/MotdSerializer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,7 +29,6 @@ namespace InetServer.Message
             var fi = new FileInfo(Filename);
             if (fi.Length > 80)
                 Logger.Warning("Message of the day longer than 80 characters, will be truncated");
-
             byte[] buffer = new byte[80];
             using (var fs = new FileStream(Filename, FileMode.Open, FileAccess.Read))
                 fs.Read(buffer, 0, 80);
