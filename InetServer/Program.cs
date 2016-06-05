ff53944950008d9a903442771e7a3129063dcc78
Emil Gedda <emil.gedda@emilgedda.se>
2016-06-05 23:15:35 +0200

    prågrajm

--- InetServer/Program.cs
+++ InetServer/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using InetServer.Network;
 
 namespace InetServer
 {
     internal class Program
     {
+        // ReSharper disable once AccessToDisposedClosure
+        /// <summary>
+        ///     The entrypoint of the program, spins up a server a server and waits for Ctrl+C to exit.
+        /// </summary>
         private static void Main()
         {
             using (var s = new Server())
             {
-                Console.CancelKeyPress += (sender, args) => s?.Dispose();
+                Console.CancelKeyPress += (sender, args) => s.Dispose();
                 s.Listen();
                 Console.ReadKey(true);
             }
