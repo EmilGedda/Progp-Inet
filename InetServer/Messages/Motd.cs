9e8dae9eb0be4337181c7549937a5212a0072094
Emil Gedda <emil.gedda@emilgedda.se>
2016-06-06 10:02:33 +0200

    Documented Motd

--- InetServer/Messages/Motd.cs
+++ InetServer/Messages/Motd.cs
@@ -3,26 +3,45 @@ using System.Text;
 
 namespace InetServer.Messages
 {
-    class Motd : Message
+    /// <summary>
+    ///     Represents the Motd as a message
+    /// </summary>
+    internal class Motd : Message
     {
-        public string Message { get; }
-
+        /// <summary>
+        ///     Construct and loads the current motd
+        /// </summary>
         public Motd()
         {
-            MotdSerializer serializer = new MotdSerializer();
+            var serializer = new MotdSerializer();
             Message = serializer.LoadMotd();
         }
 
+        /// <summary>
+        ///     Construct a Motd object from a given string
+        /// </summary>
+        /// <param name="msg">The Motd of chosing</param>
         public Motd(string msg)
         {
             Message = msg;
         }
 
+        /// <summary>
+        ///     Construct a Motd from a serialized version of this object
+        /// </summary>
+        /// <param name="p">The byte[] representing a Motd object</param>
         public Motd(byte[] p)
         {
-            Message = Encoding.UTF8.GetString(p, 1, p.Length-1);
+            Message = Encoding.UTF8.GetString(p, 1, p.Length - 1);
         }
 
+        public string Message { get; }
+
+        /// <summary>
+        ///     Serialize this object to a byte[].
+        ///     Used in communion with <see cref="Motd(byte[])" /> to serialize and deserialize Motd object.
+        /// </summary>
+        /// <returns>The byte[] corresponding to this object</returns>
         public override byte[] Destruct()
         {
             var m = Encoding.UTF8.GetBytes(Message);
