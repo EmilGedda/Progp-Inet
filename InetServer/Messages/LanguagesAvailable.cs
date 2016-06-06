6a500a5da5b7a5fb47de902f5b6e59085fd412a8
Emil Gedda <emil.gedda@emilgedda.se>
2016-06-06 10:30:47 +0200

    Documented LanguagesAvailable

--- InetServer/Messages/LanguagesAvailable.cs
+++ InetServer/Messages/LanguagesAvailable.cs
@@ -1,20 +1,38 @@
 ﻿namespace InetServer.Messages
 {
+    /// <summary>
+    ///     A message describing when a Client requests all Messages available.
+    ///     The server responds with a LanguagesAvailable containing the count of the forthcoming Languages
+    ///     and then proceeds to send said Languages.
+    /// </summary>
     public class LanguagesAvailable : Message
     {
-        public byte Count { get; }
-
+        /// <summary>
+        ///     Construct a LanguagesAvailable from a byte[] message
+        /// </summary>
+        /// <param name="payload">The serialized version of a LanguagesAvailable message</param>
         public LanguagesAvailable(byte[] payload)
         {
             Count = payload[1];
         }
 
+        /// <summary>
+        ///     Create a LanguagesAvailable object
+        /// </summary>
+        /// <param name="count">The amount of languages the server is about to send, defaults to zero.</param>
         public LanguagesAvailable(byte count = 0)
         {
             Count = count;
         }
 
-        // Empty placeholder, since the server sends all languages at once upon request.
+        public byte Count { get; }
+
+        /// <summary>
+        ///     <see cref="Message.Destruct" />
+        /// </summary>
+        /// <returns>
+        ///     <see cref="Message.Destruct" />
+        /// </returns>
         public override byte[] Destruct()
         {
             var buf = new byte[10];
